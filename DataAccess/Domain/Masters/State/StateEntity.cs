8075296d208bfe5989f9c82a5dddcab2bbfd1d73
udaymathur14 <udaymathur14@gmail.com>
2025-05-30 17:04:48 +0530

    change in teh state controller

--- DataAccess/Domain/Masters/State/StateEntity.cs
+++ DataAccess/Domain/Masters/State/StateEntity.cs
@@ -6,10 +6,13 @@ namespace DataAccess.Domain.Masters.State
     [Table("STATE_MST_TB")]
     public class StateEntity : EntityBase
     {
+        [Column("StateName")]
         [StringLength(255)]
         public string? StateName { get; set; }
 
         [StringLength(50)]
+        [Column("CountryName")]
         public string? CountryName { get; set; }
+
     }
 }
